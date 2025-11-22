b67c669d734ea4271281ed4431dee6e475f31a16
Arsen Klevtsovskyi <avklevtsovskiy@gmail.com>
2025-11-22 15:33:29 +0200

    T11

---
+++ _CMATH_2025-2026_i/T11_WebClients/A11_01.java
@@ -0,0 +1,59 @@
+package T11_WebClients;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class A11_01 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String site = "https://slovnyk.ua";
+        String word = in.next();
+        String url = site + "/?swrd=" + URLEncoder.encode(word, StandardCharsets.UTF_8);
+
+        String html = getHTML(url);
+        String result = parse(html);
+        System.out.println(result);
+    }
+
+    public static String parse(String html) {
+        Pattern p = Pattern.compile(
+                "<div class=\"toggle-content\".*?>(.*?)</div>",
+                Pattern.DOTALL
+        );
+        Matcher m = p.matcher(html);
+        if (m.find()) {
+            return m.group(1).trim().replaceAll("<.*?>", "");
+        }
+        return "";
+    }
+
+    public static String getHTML(String url) {
+        URI uri = URI.create(url);
+        try (HttpClient client = HttpClient.newHttpClient()) {
+            HttpRequest request = HttpRequest.newBuilder(uri)
+                    .GET()
+                    .header("user-agent", "Hello!")
+                    .build();
+            HttpResponse<String> response = client.send(
+                    request, HttpResponse.BodyHandlers.ofString()
+            );
+//            System.out.println(response.statusCode());
+//            System.out.println(response.headers());
+//            System.out.println(response.body());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
